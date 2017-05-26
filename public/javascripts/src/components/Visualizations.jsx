1832b4bf3b8089faf15b10a715a2d1c2b72948c4
Ganesh Iyer <lastlegion@gmail.com>
2017-05-26 16:39:29 -0400

    es6-ified visualizations.jsx

--- public/javascripts/src/components/Visualizations.jsx
+++ public/javascripts/src/components/Visualizations.jsx
@@ -3,7 +3,7 @@ var Visualization = require("./Visualizations/Visualization.jsx");
 var React = require("react");
 var ReactBootstrap = require("react-bootstrap");
 
-var Summary = require("./Visualizations/Summary.jsx");
+//var Summary = require("./Visualizations/Summary.jsx");
 
 var Tabs      = ReactBootstrap.Tabs,
     Tab         = ReactBootstrap.Tab,
@@ -12,118 +12,104 @@ var Tabs      = ReactBootstrap.Tabs,
 
 
 
-    
 
-var OptionsBar = React.createClass({
-    getInitialState: function(){
+class OptionsBar extends React.Component {
+  constructor(props){
+    super(props);
+    //console.log("woot");
+    return { showModal: false };
+  }
+  close() {
+    this.setState({ showModal: false });
+  }
+  open() {
+    this.setState({ showModal: true });
+  }
+  render() {
 
-        //console.log("woot");
-        return { showModal: false };
-    },
+    //var self = this;
+    //console.log(self.state.showModal);
+    //console.log(this.props.currData);
+    var attributes = [];
 
-    close: function(){
-        this.setState({ showModal: false });
-    },
+    for(var i in this.props.currData){
+      if(this.props.currData.hasOwnProperty(i)){
+        attributes.push(i);
+      }
+    }
 
-    open: function(){
-        this.setState({ showModal: true });
-    },
-    render: function(){
+    var url = "/save?attributes={list:"+(attributes)+"}";
 
-        //var self = this;
-        //console.log(self.state.showModal);
-        //console.log(this.props.currData);
-        var attributes = [];
+    var Attributes = attributes.map(function(d){
+      return(
+          <input type="checkbox" value="{d}">{d}</input>
+          );
+    });
+    return(
+      <div>
+        <div id="OptionsBar" className="modal-container">
+        <Button bsStyle="success" title="Download data" onClick={this.open}> Download</Button>
+        </div>
+        {
 
-        for(var i in this.props.currData){
-            attributes.push(i);
+          this.state.showModal ?
+            <Modal show={false} onHide={this.close}>
+            <h1>Download data</h1>
+
+            {Attributes}
+
+          <br />
+            <a href={url}><Button>Download</Button></a>
+            <Button onClick={this.close}>Close</Button>
+
+            </Modal>
+            :
+            <div />
         }
+      </div>
+    );
+  }
+}
 
-        var url = "/save?attributes={list:"+(attributes)+"}";
+class Visualizations extends React.Component {
+  render() {
 
-        var Attributes = attributes.map(function(d){
-            return(
-                <input type="checkbox" value="{d}">{d}</input>
-            );
-        });
+    var self  = this;
+
+    if(this.props.config){
+
+      var count=0;
+      var visualizations = this.props.config.map(function(visualization){
+
+        count++;   
+        if(!visualization.tabTitle){
+          visualization.tabTitle = visualization.visualizationType;
+        }
         return(
-            <div>
-            <div id="OptionsBar" className="modal-container">
-                <Button bsStyle="success" title="Download data" onClick={this.open}> Download</Button>
+            <Tab title={visualization.tabTitle} eventKey={count} key={count} id={visualization.tabTitle}>
+            <div className="visualizationArea">
+            <Visualization config ={visualization} debug={self.props.debug} currData={self.props.currData}  />
             </div>
-            {
+            </Tab>
+            );            
+      });
 
-                this.state.showModal ?
-                    <Modal show={false} onHide={this.close}>
-                            <h1>Download data</h1>
-                            
-                            {Attributes}
-                            
-                            <br />
-                            <a href={url}><Button>Download</Button></a>
-                            <Button onClick={this.close}>Close</Button>
 
-                    </Modal>
-                :
-                    <div />
-            }
+      return(
+        <div id="visualization_wrapper">
+          <div id="visualization" className="">
+            <Tabs defaultActiveKey={1} id="tabs">
+              {visualizations}
+            </Tabs>
+          </div>
+        </div>
+      );
 
-            </div>
-        );
     }
-});
-
-var Visualizations = React.createClass({
-    render: function(){
-        //console.log("Visualizaitons");
-        var self  = this;
-
-        if(this.props.config){
-        
-            var count=0;
-            var visualizations = this.props.config.map(function(visualization){
-                //console.log(visualization);
-                //console.log(TabPane);
-                count++;   
-                //console.log(count);
-                //console.log(visualization.tabTitle);
-                if(!visualization.tabTitle)
-                    visualization.tabTitle = visualization.visualizationType;
-                return(
-                    <Tab title={visualization.tabTitle} eventKey={count} key={count} id={visualization.tabTitle}>
-                        <div className="visualizationArea">
-                        <Visualization config ={visualization} debug={self.props.debug} currData={self.props.currData}  />
-                        </div>
-                    </Tab>
-                );            
-            });
-
-
-            /*
-             *                    <!--
-                    <div id="summary">
-                    <Summary />
-                    </div>
-                    -->
-            
-            */        
-            return(
-                <div id="visualization_wrapper">
-
-                    <div id="visualization" className="">
-                       
-                        <Tabs defaultActiveKey={1} id="tabs">
-                            {visualizations}
-                        </Tabs>
-                    </div>
-                </div>
-            );
-
-        }
-        return (
-            <div></div>
-        );
-    }
-});
+    return (
+      <div></div>
+    );
+  }
+}
 
 module.exports = Visualizations;
